ca5181cee5efc3ff63aa7fb81ad7fe1c20d4c44d
Thomas Curvelo <50064098+Thomicurve@users.noreply.github.com>
2022-04-18 17:46:52 -0300

    ejercicio 4 finished

--- ejercicio 4/script.js
+++ ejercicio 4/script.js
@@ -32,6 +32,23 @@ const stations = {
             finalPos: 200,
             height: 30000
         },
+    ], 
+    Vexyhost: [
+        {
+            initialPos: 20, //km
+            finalPos: 40, //km
+            height: 45000 //km
+        },
+        {
+            initialPos: 22,
+            finalPos: 60,
+            height: 34000
+        },
+        {
+            initialPos: 40, //km
+            finalPos: 80, //km
+            height: 40000 //km
+        },
     ]
 }
 const seconds = 10;
@@ -41,25 +58,32 @@ const dangerVelocity = 10; // km/seg
 const stationsKeys = Object.keys(stations);
 
 const dangerObjects = {};
+const dangerObjectsAmount = [];
 
-for(let station of stationsKeys) {
+for (let station of stationsKeys) {
     let heightAcum = 0;
 
     stations[station].forEach(obj => {
         const velocity = (obj.finalPos - obj.initialPos) / seconds;
 
         heightAcum += obj.height;
-        
-        if(obj.height <= dangerHeight || velocity >= dangerVelocity) 
+
+        if (obj.height <= dangerHeight || velocity >= dangerVelocity)
             !dangerObjects[station] ? dangerObjects[station] = [obj] : dangerObjects[station].push(obj);
     });
 
-    const heightAverage = heightAcum / stations[station].length ;
+    const heightAverage = heightAcum / stations[station].length;
+
+    dangerObjectsAmount.push(dangerObjects[station].length);
 
     console.log(`Estacion ${station}:`);
-    console.log(`Cantidad de objetos peligrosos: ${dangerObjects[station].length}`); 
+    console.log(`Cantidad de objetos peligrosos: ${dangerObjects[station].length}`);
     console.log(`Objetos peligrosos de la estacion:`, dangerObjects[station]);
     console.log(`Promedio de altura de objetos: ${heightAverage}`);
     console.log('---------------------------------------------------------------------------------------------');
 };
+
+const highDangerousObj = stationsKeys[dangerObjectsAmount.indexOf(Math.max(...dangerObjectsAmount))];
+console.log('La estacion con mas objetos peligrosos es: ', highDangerousObj);
+console.log('---------------------------------------------------------------------------------------------');
 
